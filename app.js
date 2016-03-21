52bfffabc6dd6bed09f8274cf52ed9c0355524e8
ivanhuay <ivan.huayrana@despegar.com>
2016-03-21 15:02:11 -0300

    cambio en la logica de movimiento generico en el servidor

--- app.js
+++ app.js
@@ -19,65 +19,103 @@ function handler(req, res) {
         });
 }
 
-var ColorStore = function(){
-	this.people = {};
-	var self = this;
-	this.addPeople = function(peopleId){
-		self.people[peopleId] = "rgb("+Math.round(Math.random()*255)+","+Math.round(Math.random()*255)+","+Math.round(Math.random()*255)+")";
-	}
+var PeopleStore = function() {
+    this.people = {};
+    var self = this;
+    this.addPeople = function(peopleId) {
+        self.people[peopleId] = {
+            top: 100,
+            left: 100,
+            color: "rgb(" + Math.round(Math.random() * 255) + "," + Math.round(Math.random() * 255) + "," + Math.round(Math.random() * 255) + ")"
+        };
+    }
 
-	this.peopleColor = function(peopleId){
-		return self.people[peopleId];
-	}
+    this.peopleColor = function(peopleId) {
+        return self.people[peopleId].color;
+    }
+
+    this.getPosition = function(peopleId) {
+        return {
+            top: self.people[peopleId].top,
+            left: self.people[peopleId].left
+        };
+    }
+
+    this.moveY = function(peopleId, _mov) {
+        self.people[peopleId].top += _mov;
+    }
+
+    this.moveX = function(peopleId, _mov) {
+        self.people[peopleId].left += _mov;
+    }
+
+    this.checkColition = function(peopleId){
+    	var _top = self.people[peopleId].top;
+    	var _left = self.people[peopleId].left;
+    	for(var sockId in self.people){
+    		if(peopleId != sockId){
+    			sockPosition = self.getPosition(sockId);
+    			// if(_top > sockPosition.)
+    		}
+    	}
+    }
 }
 
-var store = new ColorStore();
+var store = new PeopleStore();
 
 io.on('connection', function(socket) {
     var cleanSocketId = socket.id.replace("/#", "");
     var mov = 10;
     store.addPeople(cleanSocketId);
-    console.log(cleanSocketId+"---> "+store.peopleColor(cleanSocketId));
 
-    socket.emit('createBox', {id:cleanSocketId,color:store.peopleColor(cleanSocketId)});
+    socket.broadcast.emit('createPeople', {
+        id: cleanSocketId,
+        color: store.peopleColor(cleanSocketId),
+        position: store.getPosition(cleanSocketId)
+    });
 
-    socket.broadcast.emit('createPeople', {id:cleanSocketId,color:store.peopleColor(cleanSocketId)});
 
-    socket.on("up", function(position) {
-        position.top += mov;
+    socket.on("up", function() {
+        store.moveY(cleanSocketId, mov);
         io.sockets.emit("move", {
-            position: position,
+            position: store.getPosition(cleanSocketId),
             id: cleanSocketId
         });
     });
-    socket.on("down", function(position) {
-        position.top -= mov;
+
+    socket.on("down", function() {
+        store.moveY(cleanSocketId, -mov);
         io.sockets.emit("move", {
-            position: position,
+            position: store.getPosition(cleanSocketId),
             id: cleanSocketId
         });
     });
-    socket.on("left", function(position) {
-        position.left -= mov;
+
+    socket.on("left", function() {
+        store.moveX(cleanSocketId, -mov);
         io.sockets.emit("move", {
-            position: position,
+            position: store.getPosition(cleanSocketId),
             id: cleanSocketId
         });
     });
-    socket.on("right", function(position) {
-        position.left += mov;
+
+    socket.on("right", function() {
+        store.moveX(cleanSocketId, mov);
         io.sockets.emit("move", {
-            position: position,
+            position: store.getPosition(cleanSocketId),
             id: cleanSocketId
         });
     });
-    socket.on('disconnect',function(){
-    	socket.broadcast.emit("deletePeople",cleanSocketId);
+
+    socket.on('disconnect', function() {
+        socket.broadcast.emit("deletePeople", cleanSocketId);
     });
 
     for (var socketId in io.sockets.sockets) {
-	    if(socket.id != socketId){
-    		socket.emit('createPeople', {id:socketId.replace("/#", ""),color:store.peopleColor(socketId.replace("/#", ""))});
-	    } 
-	}
+        socket.emit('createPeople', {
+            id: socketId.replace("/#", ""),
+            color: store.peopleColor(socketId.replace("/#", "")),
+            position: store.getPosition(socketId.replace("/#", ""))
+        });
+    }
 });
